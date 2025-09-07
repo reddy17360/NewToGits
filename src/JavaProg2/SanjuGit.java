c4870ac080745044a82c1d0b6f49c045152a13ee
Reddy <sanjeevreddy9611@gmail.com>
2025-09-07 11:21:47 +0530

    done

--- src/JavaProg2/SanjuGit.java
+++ src/JavaProg2/SanjuGit.java
@@ -5,6 +5,7 @@ public class SanjuGit {
     public static void main( String args[]){
 
         System.out.println("adding from java programs users");
+        System.out.println("adding from java programs users");
 
     }
 }
